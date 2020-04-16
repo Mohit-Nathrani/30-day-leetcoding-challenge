f5e10d34d454ceefc350d5b8189a29a90d4a2e6b
mohit-nathrani <mohitkn01@gmail.com>
2020-04-16 15:56:21 +0530

    day 15 solution

---
+++ 15_product_of_array_except_self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> res(n,1);
+        int prev=1;
+        for(int i=0;i<n;i++){
+            res[i]*=prev;
+            prev*=nums[i];
+        }
+        prev=1;
+        for(int i=n-1;i>=0;i--){
+            res[i]*=prev;
+            prev*=nums[i];
+        }
+        return res;
+    }
+};
